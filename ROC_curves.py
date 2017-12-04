35eafa9c2f9b8408c841529a5fdebaef1dbbcd6f
Lewis Smith <lewis.smith@eng.ox.ac.uk>
2017-12-04 20:40:45 +0000

    Modified formatting to be more pep8 compliant

--- ROC_curves.py
+++ ROC_curves.py
@@ -1,19 +1,19 @@
-import tensorflow as tf
-import keras
-import numpy as np
-import matplotlib as mpl
-mpl.use('Agg')
-from matplotlib import pyplot as plt
-from keras.models import load_model, save_model
-from keras import backend as K
 import itertools as itr
+import tensorflow as tf
 from cleverhans.attacks_tf import fgm
 import os
 import sys
-
+import keras
+import numpy as np
+from keras.models import load_model, save_model
+from keras import backend as K
 from sklearn.metrics import roc_curve, roc_auc_score
 from src.utilities import *
 import argparse
+import matplotlib as mpl
+mpl.use('Agg')
+from matplotlib import pyplot as plt
+
 
 """
 This script compares the ROC for entropy and BALD for fgm with a given norm at
@@ -22,23 +22,19 @@ sample of the adverserial examples generated for visualisation.
 """
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--eps_min', type = float, default = 0.1,
-                    help = "Minimum value of epsilon to generate \
-                    adverserial examples with FGM")
-parser.add_argument('--eps_max', type = float, default = 1,
-                    help = "Max value of epsilon to generate \
-                    adverserial examples with")
-parser.add_argument('--N_eps', type = float, default = 10,
-                    help = "Number of values of epsilon to use \
-                    (linspace eps_min eps_max)")
-parser.add_argument('--N_data', type = int, default = 100,
-                    help = "Number of examples of adverserial and \
-                    non-adverserial examples to use. If 0 will use the entire \
-                    dataset")
-parser.add_argument('--norm', default = 'inf',
-                    help = "which norm to use: currently <- {1,2,inf}")
-parser.add_argument('--N_mc', default = 50, type = int,
-                    help = "Number of MC forward passes to use.")
+parser.add_argument('--eps_min', type=float, default=0.1, help="Minimum value \
+    of epsilon to generate adverserial examples with FGM")
+parser.add_argument('--eps_max', type=float, default=1, help="Max value of \
+    epsilon to generate adverserial examples with")
+parser.add_argument('--N_eps', type=float, default=10, help="Number of values \
+    of epsilon to use (linspace eps_min eps_max)")
+parser.add_argument('--N_data', type=int, default=100, help="Number of examples \
+    of adverserial and non-adverserial examples to use. If 0 will use the \
+    entire dataset")
+parser.add_argument('--norm', default='inf', help="which norm to use: \
+    currently <- {1,2,inf}")
+parser.add_argument('--N_mc', default=50, type=int, help="Number of MC forward \
+    passes to use.")
 
 args = parser.parse_args()
 
@@ -52,37 +48,39 @@ else:
     raise NotImplementedError("Norms other than 1,2, inf not implemented")
 
 
-eps = np.linspace(args.eps_min,args.eps_max,args.N_eps)
+eps = np.linspace(args.eps_min, args.eps_max, args.N_eps)
 SYNTH_DATA_SIZE = args.N_data
 
 x_test, y_test, x_train, y_train = get_mnist()
 
 
 K.set_learning_phase(True)
-#load the pre-trained model (trained by another file)
+# load the pre-trained model (trained by another file)
 model = load_model('mnist_cnn.h5')
 
 n_mc = args.N_mc
 
-x = K.placeholder(shape = [None] +  list(x_test.shape[1:]))
+x = K.placeholder(shape=[None] + list(x_test.shape[1:]))
 mc_preds_tensor = mc_dropout_preds(model, x, n_mc)
-mean_entropy_tensor  = m_entropy(mc_preds_tensor)
-bald_tensor     = BALD(mc_preds_tensor)
+mean_entropy_tensor = m_entropy(mc_preds_tensor)
+bald_tensor = BALD(mc_preds_tensor)
 get_output = K.function([x], [mc_preds_tensor,
                               mean_entropy_tensor,
                               bald_tensor])
 
 
-preds_tensor = K.mean(mc_preds_tensor, axis = 0)
+preds_tensor = K.mean(mc_preds_tensor, axis=0)
 
-#create a synthetic training set at various epsilons, and evaluate the ROC curves on it
+# create a synthetic training set at various epsilons, 
+# and evaluate the ROC curves on it
 
 x_real = x_test[np.random.randint(x_test.shape[0], size=SYNTH_DATA_SIZE)]
 x_to_adv = x_test[np.random.randint(x_test.shape[0], size=SYNTH_DATA_SIZE)]
 
-x_advs_plot = [tile_images([x_to_adv[i] for i in range(10)], horizontal = False) ]
+x_advs_plot = [tile_images([x_to_adv[i] for i in range(10)], horizontal=False)]
 
-x_real_label = [0 for _ in range(SYNTH_DATA_SIZE)] #label zero for non adverserial input
+# label zero for non adverserial input
+x_real_label = [0 for _ in range(SYNTH_DATA_SIZE)]
 x_adv_label = [1 for _ in range(SYNTH_DATA_SIZE)]
 
 
@@ -91,64 +89,61 @@ bald_aucs = []
 H_aucs = []
 
 plt.figure()
-for i,ep in enumerate(eps):
+for i, ep in enumerate(eps):
 
-
-    #log progress
+    # log progress
     print("iteration {} of {}, ep = {}".format(i, len(eps), ep))
-    sys.stdout.flush() #force a write if we have redirected stdout
+    sys.stdout.flush()  # force a write if we have redirected stdout
 
+    adv_tensor = fgm(x, preds_tensor, eps=ep, ord=norm, clip_min=0, clip_max=1)
 
-    adv_tensor = fgm(x, preds_tensor, eps = ep, ord = norm, clip_min = 0, clip_max = 1)
+    x_adv = adv_tensor.eval(session=K.get_session(),
+                            feed_dict={x: x_to_adv})
 
-
-
-    x_adv = adv_tensor.eval(session = K.get_session(),
-                            feed_dict = {x: x_to_adv})
-
-
-    #calculate the L-norm distances between the adv examples and the originals
-    dists = batch_L_norm_distances(x_to_adv, x_adv, ord = norm)
+    # calculate the L-norm distances between the adv examples and the originals
+    dists = batch_L_norm_distances(x_to_adv, x_adv, ord=norm)
     adv_distances.append(dists.mean())
-
 
     x_synth = np.concatenate([x_real, x_adv])
     y_synth = np.array(x_real_label + x_adv_label)
 
-
-    #save the adverserial examples to plot
+    # save the adverserial examples to plot
     x_advs_plot.append(tile_images([x_adv[i] for i in range(10)],
-                        horizontal = False))
+                                   horizontal=False))
 
-
-    #get the entropy and bald on this task
+    # get the entropy and bald on this task
 
     _, entropy, bald = get_output([x_synth])
 
-    fpr_entropy, tpr_entropy,_ = roc_curve(y_synth, entropy, pos_label = 1)
-    fpr_bald, tpr_bald, _      = roc_curve(y_synth, bald,    pos_label = 1)
+    fpr_entropy, tpr_entropy, _ = roc_curve(y_synth, entropy, pos_label=1)
+    fpr_bald, tpr_bald, _ = roc_curve(y_synth, bald,    pos_label=1)
 
     AUC_entropy = roc_auc_score(y_synth, entropy)
-    AUC_bald    = roc_auc_score(y_synth, bald)
+    AUC_bald = roc_auc_score(y_synth, bald)
 
     H_aucs.append(AUC_entropy)
     bald_aucs.append(AUC_bald)
 
-    plt.clf() #avoid making too many figures by resuing the same one
-    plt.plot(fpr_entropy, tpr_entropy,
-             label = "Entropy, AUC: {}".format(AUC_entropy))
-    plt.plot(fpr_bald, tpr_bald,
-             label = "BALD, AUC: {}".format(AUC_bald))
+    plt.clf()  # avoid making too many figures by resuing the same one
+    plt.plot(
+        fpr_entropy,
+        tpr_entropy,
+        label="Entropy, AUC: {}".format(AUC_entropy))
+    plt.plot(
+        fpr_bald,
+        tpr_bald,
+        label="BALD, AUC: {}".format(AUC_bald))
     plt.xlabel('FPR')
     plt.ylabel('TPR')
     plt.legend()
     plt.savefig(os.path.join("output", "ROC_eps_{}.png".format(ep)))
 
-#plot the adverserial images
+# plot the adverserial images
 plt.figure()
-tile = tile_images(x_advs_plot, horizontal = True)
-plt.imshow(tile, cmap = 'gray_r')
-plt.savefig(os.path.join("output", "adv_images_ep_{}_to_{}.png".format(eps.min(), eps.max())))
+tile = tile_images(x_advs_plot, horizontal=True)
+plt.imshow(tile, cmap='gray_r')
+plt.savefig(os.path.join(
+    "output", "adv_images_ep_{}_to_{}.png".format(eps.min(), eps.max())))
 
 plt.figure()
 plt.plot(eps, adv_distances)
@@ -157,8 +152,8 @@ plt.ylabel("Average L{} distance of advererial images".format(norm))
 plt.savefig(os.path.join("output", "{}_norm_eps_vs_avg_dist.png".format(norm)))
 
 plt.figure()
-plt.plot(eps, H_aucs, label = "Entropy")
-plt.plot(eps, bald_aucs, label = "BALD")
+plt.plot(eps, H_aucs, label="Entropy")
+plt.plot(eps, bald_aucs, label="BALD")
 plt.xlabel('FGM Epsilon')
 plt.ylabel('AUC')
 plt.legend()
--- bald_vs_entropy.py
+++ bald_vs_entropy.py
@@ -17,21 +17,18 @@ This script calculates adversarial examples using fgm for a range of step-sizes
 epsilon, and plots the entropy and the BALD score at all of them.
 """
 parser = argparse.ArgumentParser()
-parser.add_argument('--eps_min', type = float, default = 0.1,
-                    help = "Minimum value of epsilon to generate \
-                    adverserial examples with FGM")
-parser.add_argument('--eps_max', type = float, default = 1,
-                    help = "Max value of epsilon to generate \
-                    adverserial examples with")
-parser.add_argument('--N_eps', type = float, default = 50,
-                    help = "Number of values of epsilon to use \
-                    (linspace eps_min eps_max)")
-parser.add_argument('--N_data', type = int, default = 100,
-                    help = "Number of examples from the training set to use")
-parser.add_argument('--norm', default = 'inf',
-                    help = "which norm to use: currently <- {1,2,inf}")
-parser.add_argument('--N_mc', default = 50, type = int,
-                    help = "Number of MC forward passes to use.")
+parser.add_argument('--eps_min', type=float, default=0.1, help="Minimum value \
+    of epsilon to generate adverserial examples with FGM")
+parser.add_argument('--eps_max', type=float, default=1, help="Max value of \
+    epsilon to generate adverserial examples with")
+parser.add_argument('--N_eps', type=float, default=50, help="Number of values \
+    of epsilon to use (linspace eps_min eps_max)")
+parser.add_argument('--N_data', type=int, default=100, help="Number of examples \
+    from the training set to use")
+parser.add_argument('--norm', default='inf', help="which norm to use: \
+    currently <- {1,2,inf}")
+parser.add_argument('--N_mc', default=50, type=int, help="Number of MC forward \
+    passes to use.")
 
 args = parser.parse_args()
 
@@ -39,10 +36,11 @@ x_test, y_test, x_train, y_train = get_mnist()
 
 
 K.set_learning_phase(True)
-#load the pre-trained model (trained using this script https://github.com/yaringal/acquisition_example)
+# load the pre-trained model 
+# (trained using this script https://github.com/yaringal/acquisition_example)
 model = load_model('mnist_cnn.h5')
 
-eps = np.linspace(args.eps_min,args.eps_max, args.N_eps)
+eps = np.linspace(args.eps_min, args.eps_max, args.N_eps)
 if args.norm == 'inf':
     norm = np.inf
 elif args.norm == '1':
@@ -52,60 +50,61 @@ elif args.norm == '2':
 else:
     raise NotImplementedError("Norms other than 1,2, inf not implemented")
 
-#could be an idea to shuffle before we do this later on.
+# could be an idea to shuffle before we do this later on.
 tst = x_test[:args.N_data]
 tsty = y_test[:args.N_data]
 n_mc = args.N_mc
 
-x = K.placeholder(shape = [None] +  list(x_test.shape[1:]))
+x = K.placeholder(shape=[None] + list(x_test.shape[1:]))
 mc_preds_tensor = mc_dropout_preds(model, x, n_mc)
-mean_entropy_tensor  = m_entropy(mc_preds_tensor)
-bald_tensor     = BALD(mc_preds_tensor)
+mean_entropy_tensor = m_entropy(mc_preds_tensor)
+bald_tensor = BALD(mc_preds_tensor)
 get_output = K.function([x], [mc_preds_tensor,
                               mean_entropy_tensor,
                               bald_tensor])
 
 
-#plot entropy and MI as a eps increases
+# plot entropy and MI as eps increases
 
 entropies = []
 balds = []
 accs = []
 
-preds_tensor = K.mean(mc_preds_tensor, axis = 0)
+preds_tensor = K.mean(mc_preds_tensor, axis=0)
 
 
 for i, ep in enumerate(eps):
 
-    print("iteration",i,"of", len(eps), "epsilon", ep)
+    print("iteration", i, "of", len(eps), "epsilon", ep)
     sys.stdout.flush()
 
-    adv_tensor = fgm(x, preds_tensor, eps = ep, clip_min = 0, clip_max = 1, ord = norm)
+    adv_tensor = fgm(x, preds_tensor, eps=ep, clip_min=0, clip_max=1, ord=norm)
     b_entropies = []
-    b_balds    = []
-    b_accs     = []
+    b_balds = []
+    b_accs = []
 
-    batches = batches_generator(tst, tsty, batch_size = 500)
-    for j,(bx, by) in enumerate(batches):
+    batches = batches_generator(tst, tsty, batch_size=500)
+    for j, (bx, by) in enumerate(batches):
 
         print('    batch', j)
-        sys.stdout.flush() #in case we are writing to a log file not stdout
+        sys.stdout.flush()  # in case we are writing to a log file not stdout
 
-        adv = adv_tensor.eval(session = K.get_session(), feed_dict = {x: bx})
+        adv = adv_tensor.eval(session=K.get_session(), feed_dict={x: bx})
         mc_samples, e_adv, b_adv = get_output([adv])
-        b_entropies.append(e_adv.mean() ) #mean across the batch
-        b_balds.append(b_adv.mean())      #ditto
-        preds = mc_samples.mean(axis = 0)         #mean across the mc samples per point
-        b_accs.append( np.mean(np.equal(preds.argmax(axis=1),
-                                        by.argmax(axis=1)) ))
-                                        #mean accuracy across the batch
-    entropies.append( np.array(b_entropies).mean() ) #mean across all batches
-    balds.append(  np.array(b_balds).mean() ) #ditto
-    accs.append(  np.array(b_accs).mean())          #ditto
+        b_entropies.append(e_adv.mean())  # mean across the batch
+        b_balds.append(b_adv.mean())  # ditto
+        preds = mc_samples.mean(axis=0)  # mean across the mc samples per point
+        b_accs.append(np.mean(np.equal(preds.argmax(axis=1),
+                                       by.argmax(axis=1))))
+        # mean accuracy across the batch
+    entropies.append(np.array(b_entropies).mean())  # mean across all batches
+    balds.append(np.array(b_balds).mean())  # ditto
+    accs.append(np.array(b_accs).mean())  # ditto
 
 
 to_plot = [entropies, balds, accs]
 names = ['Entropy', 'BALD', 'Accuracy']
+
 
 def mk_plot(value, name):
 
@@ -114,8 +113,9 @@ def mk_plot(value, name):
     plt.plot(eps, value)
     plt.xlabel('FGSM Epsilon')
     plt.ylabel(name)
-    figname = "".join(["fgsm_untargeted_",name,".png"])
+    figname = "".join(["fgsm_untargeted_", name, ".png"])
     plt.savefig(os.path.join(outdir, figname))
+
 
 for v, n in zip(to_plot, names):
     mk_plot(v, n)
