83909433093ba5de1d3fe6a3f5126d2450f0e21e
Lewis Smith <lewis.smith@eng.ox.ac.uk>
2018-01-04 22:20:54 +0000

    add mcmc code to generate weights instead of direct predictions

--- src/mcmc.py
+++ src/mcmc.py
@@ -11,6 +11,137 @@ def reset_model(model: keras.models.Model):
             layer.bias.initializer.run(session=K.get_session())
     return
 
+def HMC_ensemble_run(model: keras.models.Model,
+            x_train: np.array,
+            y_train: np.array,
+            N_mc: int,
+            ep: float,
+            tau: int,
+            burn_in: int,
+            sample_every: int,
+            return_extra=False,
+            verbose=True,
+            ):
+    """
+    Takes a keras model and a dataset (x_train, y_train) and returns a list of numpy
+    arrays to be used as weights for an ensemble predictor.
+    """
+    Ws = model.weights
+    lossfn = model.loss #this is kinda cheeky
+    X = model.input
+    Y_ = model.output
+    Y = K.placeholder(shape=Y_.shape)
+    L = K.sum(lossfn(Y, Y_)) #the sum accross the batch
+    Gs = K.gradients(L, Ws)
+    eval_loss_and_grads = K.function([X,Y], [L] + Gs)
+    def get_loss_and_grads(x, y):
+        res = eval_loss_and_grads([x, y])
+        return res[0], res[1:]
+    losses = []
+    i = 0
+    weights = []
+    ensemble = []
+    accept_n = 0
+    while len(ensemble) < N_mc:
+        if verbose and i % 1000 == 0:
+            print("iter: ", i)
+        i += 1 
+        obj, gs = get_loss_and_grads(x_train, y_train)
+        losses.append(obj)
+        ps = [np.random.normal(size=w.shape) for w in Ws]  # momentum variables
+
+        H = .5 * sum([np.sum(p ** 2) for p in ps]) + obj
+
+        ws = [K.eval(w) for w in Ws]
+        weights.append(ws)
+        ws_old = [K.eval(w) for w in Ws]
+        # store the values of the weights in case we need to go back
+        for t in range(tau):
+            for p, g in zip(ps, gs):
+                p - .5 * ep * g
+
+            for w, p in zip(ws, ps):
+                w += ep * p
+
+            # evaluate new weights
+            for (weight, value) in zip(Ws, ws):
+                K.set_value(weight, value)
+            obj, gs = get_loss_and_grads(x_train, y_train)
+
+            for p, g in zip(ps, gs):
+                p - .5 * ep * g
+
+        H_new = .5 * sum([np.sum(p ** 2) for p in ps]) + obj
+
+        dH = H_new - H
+
+        if (dH < 0) or np.random.rand() < np.exp(-dH):
+            # in this case, we acccept the new values
+            accept_n += 1
+            pass
+        else:
+            # reverse the step
+            for weight, value in zip(Ws, ws_old):
+                K.set_value(weight, value)
+
+        if i > burn_in and i % sample_every == 0:
+           ensemble.append([K.eval(w) for w in Ws])
+
+    if return_extra:
+        weights = np.array(weights)
+        return ensemble, losses, weights, accept_n / i
+    else:
+        return ensemble
+
+
+def HMC_ensemble( model: keras.models.Model,
+            x_train: np.array,
+            y_train: np.array,
+            N_mc=10,
+            ep=5-3,
+            tau=1,
+            burn_in=5000,
+            sample_every=1000,
+            samples_per_init=1,
+            verbose=True,):
+    ensemble = []
+    i = 0
+    while len(ensemble) < N_mc:
+        if verbose: print("run: ", i)
+        i += 1 
+        reset_model(model)
+        run_ensemble = HMC_ensemble_run(model,
+                               x_train,
+                               y_train,
+                               samples_per_init,
+                               ep,
+                               tau,
+                               burn_in,
+                               sample_every,
+                               verbose=verbose)
+        ensemble += run_ensemble 
+    return ensemble
+
+def HMC_ensemble_predict(model, ensemble_weights, test_x):
+    """
+    Given a model and an ensemble of HMC weights, get the average prediction
+    of the model
+    """
+    cma = None
+    n = 1
+    for ews in ensemble_weights:
+        weights = model.weights
+        for w, ensemble_value in zip(weights, ews):
+            K.set_value(w, ensemble_value)
+        preds = model.predict(test_x)
+        if cma is None:
+            cma = preds
+        else:
+            n += 1
+            cma += (preds - cma) / n
+    return cma
+
+
 
 def HMC_run(model: keras.models.Model,
             lossfn,  # Tensor -> Tensor -> Tensor
@@ -49,7 +180,6 @@ def HMC_run(model: keras.models.Model,
     losses = []
 
     mc_preds = []
-    n_mc = 0
     weights = []
     accept_n = 0
 
