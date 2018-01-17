bf70ae97dcf5ed6acc30aaa0d6f1648ab4459354
Lewis Smith <lewis.smith@eng.ox.ac.uk>
2018-01-17 17:29:31 +0000

    Cleaned up this script a little (__name__ == __main__)

--- load_dropout_model.py
+++ load_dropout_model.py
@@ -21,30 +21,33 @@ epochs=12
 batch_size = 128
 num_classes = 10
 
+def load_drop_model(weights_file):
+    model.add(ConcreteDropout(Conv2D(32, kernel_size=(3,3),
+        activation=act_fn),
+        input_shape=input_shape))
+    model.add(ConcreteDropout(Conv2D(64, (3,3), activation=act_fn)))
+    model.add(MaxPooling2D(pool_size=(2,2)))
+    model.add(Flatten())
+    model.add(ConcreteDropout(Dense(128, activation=act_fn)))
+    model.add(ConcreteDropout(Dense(num_classes, activation='softmax')))
 
-model.add(ConcreteDropout(Conv2D(32, kernel_size=(3,3),
-    activation=act_fn),
-    input_shape=input_shape))
-model.add(ConcreteDropout(Conv2D(64, (3,3), activation=act_fn)))
-model.add(MaxPooling2D(pool_size=(2,2)))
-model.add(Flatten())
-model.add(ConcreteDropout(Dense(128, activation=act_fn)))
-model.add(ConcreteDropout(Dense(num_classes, activation='softmax')))
+    model.load_weights(weights_file)
+    return model
 
-model.load_weights('mnist_cdrop_cnn.h5')
+if __name__ == '__main__':
+    model = load_drop_model
+    x = K.placeholder(shape=[None] + list(x_test.shape[1:]))
+    preds_tensor = U.mc_dropout_preds(model, x, 50)
 
-x = K.placeholder(shape=[None] + list(x_test.shape[1:]))
-preds_tensor = U.mc_dropout_preds(model, x, 50)
+    #evaluate in batches to avoid crashing tensorflow
 
-#evaluate in batches to avoid crashing tensorflow
+    batches = U.batches_generator(x_test, y_test, batch_size=200)
 
-batches = U.batches_generator(x_test, y_test, batch_size=200)
-
-#preds = preds_tensor.eval(session=K.get_session(), feed_dict={x: x_test}).mean(axis=0)
-accs = []
-for bx, by in batches:
-    bpreds = preds_tensor.eval(session=K.get_session(), feed_dict={x: bx}).mean(axis=0)
-    bacc = np.mean(np.equal(bpreds.argmax(axis=1), by.argmax(axis=1)))
-    accs.append(bacc)
-acc = np.mean(np.array(accs))
-print(acc)
+    #preds = preds_tensor.eval(session=K.get_session(), feed_dict={x: x_test}).mean(axis=0)
+    accs = []
+    for bx, by in batches:
+        bpreds = preds_tensor.eval(session=K.get_session(), feed_dict={x: bx}).mean(axis=0)
+        bacc = np.mean(np.equal(bpreds.argmax(axis=1), by.argmax(axis=1)))
+        accs.append(bacc)
+    acc = np.mean(np.array(accs))
+    print(acc)
