412bdcc3007fe58b72ea1b0c0135f27a419b5379
Lewis Smith <lewis.smith@eng.ox.ac.uk>
2018-02-01 09:49:16 +0000

    Updated these scripts, adding a non interactive plotting version

--- latent_plots.py
+++ latent_plots.py
@@ -60,8 +60,8 @@ def get_models():
     _, encoder, decoder = define_VAE()
     encoder.load_weights('save/enc_weights.h5')
     decoder.load_weights('save/dec_weights.h5')
-
-    model = load_drop_model('save/mnist_cdrop_cnn_run.h5')
+    K.set_learning_phase(True)
+    model = keras.models.load_model('save/mnist_cnn_run_1.h5')
     mc_model = U.MCModel(model, model.input, n_mc=50)
     #we have been using more mc samples elsewhere, but save time for now
     return mc_model, encoder, decoder
@@ -79,6 +79,22 @@ def get_model_ensemble(n_mc=10):
     mc_model = U.MCEnsembleWrapper(models, n_mc=10)
     return mc_model, encoder, decoder
 
+def get_ML_ensemble():
+    
+    _, encoder, decoder = define_VAE()
+    encoder.load_weights('save/enc_weights.h5')
+    decoder.load_weights('save/dec_weights.h5')
+    K.set_learning_phase(False)
+    ms = []
+    for name in filter(lambda x: 'mnist_cnn' in x, os.listdir('save')):
+        print('loading model {}'.format(name))
+        model = load_model('save/' + name)
+        ms.append(model)
+
+    model = U.MCEnsembleWrapper(ms, n_mc=1)
+    return model, encoder, decoder 
+
+    
 
 def get_ML_models():
     _, encoder, decoder = define_VAE()
@@ -92,7 +108,23 @@ def get_ML_models():
         ent = - np.sum(preds * np.log(preds + 1e-10), axis=-1)
         return preds, ent, np.zeros(ent.shape)
 
-    model.get_results = get_results #hacks galore
+    model.get_results = get_results 
+    return model, encoder, decoder
+
+
+def get_ML_no_drop_models():
+    _, encoder, decoder = define_VAE()
+    encoder.load_weights('save/enc_weights.h5')
+    decoder.load_weights('save/dec_weights.h5')
+
+    model = keras.models.load_model('save/mnist_cnn_no_drop_run.h5')
+
+    def get_results(X):
+        preds = model.predict(X)
+        ent = - np.sum(preds * np.log(preds + 1e-10), axis=-1)
+        return preds, ent, np.zeros(ent.shape)
+
+    model.get_results = get_results 
     return model, encoder, decoder
 
 
@@ -146,17 +178,50 @@ def make_interactive_plot(proj_x,
             last_sample = dream
     f.canvas.mpl_connect('button_press_event', on_click)
 
+def make_plot(proj_x,
+              proj_y,
+              extent,
+              plot_bg,
+              decoder,
+              model,
+              title="",
+              bgcmap='gray',
+              bgalpha=0.9,
+              sccmap='Set3'):
+    
+    f, ax = plt.subplots()
+    ax.scatter(proj_x[:,0],
+                proj_x[:,1],
+                c = proj_y.argmax(axis=1),
+                marker=',',
+                s=1,
+                cmap=sccmap,
+               alpha=0.1
+    )
+
+    ax.imshow(plot_bg,
+                 cmap=bgcmap,
+                 origin='lower',
+                 alpha=bgalpha,
+                 extent=extent,
+    )
+    ax.set_xlabel('First Latent Dimension')
+    ax.set_ylabel('Second Latent Dimension')
+    ax.set_title(title)
+
+
 if __name__ == '__main__':
 
     #model, encoder, decoder = get_models_3s_7s()
     
     #x_train, y_train, x_test, y_test = mnist_to_3s_and_7s(U.get_mnist())
-    # model, encoder, decoder = get_models()
-    model, encoder, decoder = get_model_ensemble(n_mc=20)
+    model, encoder, decoder = get_models()
+    #model, encoder, decoder = get_model_ensemble(n_mc=20)
     
-    x_train, y_train, x_test, y_test = U.get_mnist()
+   # x_train, y_train, x_test, y_test = U.get_mnist()
 
-    #model, encoder, decoder = get_ML_models()
+   # model, encoder, decoder = get_ML_models()
+    #model, encoder, decoder = get_ML_ensemble()
    # _, encoder, decoder = define_VAE()
    # encoder.load_weights('save/enc_weights.h5')
    # decoder.load_weights('save/dec_weights.h5')
@@ -164,64 +229,29 @@ if __name__ == '__main__':
    # model1 = load_drop_model('save/mnist_cdrop_cnn.h5')
 
    # model2 = keras.models.load_model('save/mnist_cnn.h5')
-
+   # model, encoder, decoder = get_ML_no_drop_models()
 
     
-    proj_x_train = encoder.predict(x_train)
+    
+    #proj_x_train = encoder.predict(x_train)
    # model = EnsembleWrapper([model1, model2])
 
     zmin, zmax = -10,10
-    n_grid = 50
+    n_grid = 100
     preds, plot_ent, plot_bald = get_uncertainty_samples(model,
                                                          encoder,
                                                          decoder,
                                                          [zmin, zmax],
                                                          n_grid=n_grid)
     
-    make_interactive_plot(proj_x_train,
-                          y_train,
-                          [zmin, zmax, zmin, zmax],
-                          plot_bald,
-                          decoder,
-                          model,
-                          )
-                          
+    make_plot(proj_x_train,
+              y_train,
+              [zmin, zmax, zmin, zmax],
+              plot_ent,
+              decoder,
+              model,
+    )
+    
     print('done')              
+    #plt.savefig('overleaf-paper/figures/ML_dropout_uncertainty.pdf')
     plt.show()
-
-    wrap = CallableModelWrapper(model, 'probs')
-    fgm = attacks.FastGradientMethod(wrap,sess=K.get_session())
-    adv_tensor = fgm.generate(model.input, eps=0.1, clip_min=0, clip_max=1)
-
-    adv = adv_tensor.eval(session=K.get_session(), feed_dict={model.input: x_test[:20]})
-
-    test_proj = encoder.predict(x_test)
-    adv_proj  = encoder.predict(adv)
-    adv_dec   = decoder.predict(adv_proj)
-
-    im = 0
-    def plot_next():
-        global im
-        f, ax = plt.subplots(3,2)
-
-        ax[0,0].imshow(x_test[im].squeeze(), cmap='gray_r')
-        ax[0,0].set_title('Original Image')
-        ax[0,1].imshow(adv[im].squeeze(), cmap='gray_r')
-        ax[0,1].set_title('Adversarial Example')
-        ax[1,0].imshow(adv_dec[im].squeeze(), cmap='gray_r')
-        ax[1,0].set_title('Reconstructed Adversarial Example')
-
-        ax[1,1].imshow(decoder.predict(test_proj[im:im+1]).squeeze(), cmap='gray_r')
-        ax[1,1].set_title('Reconstructed Original')
-        
-        ax[2,1].imshow(preds.argmax(axis=1).reshape(n_grid, n_grid),
-                       origin='lower',
-                       extent=[zmin, zmax, zmin, zmax],
-                       cmap='Set3',
-                       alpha=0.3)
-
-        ax[2,1].scatter(test_proj[im, 0], test_proj[im, 1], c='b')
-        ax[2,1].scatter(adv_proj[im, 0], adv_proj[im, 1], c='r')
-        ax[2,1].set_title('Latent Space')
-        plt.show()
-        im += 1
