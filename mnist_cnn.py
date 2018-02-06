c844c00ccbd616a714982c618e58c8f7533613f8
Lewis <lsgs@robots.ox.ac.uk>
2018-02-06 18:26:54 +0000

    modified this file to create unique saves

--- mnist_cnn.py
+++ mnist_cnn.py
@@ -13,6 +13,8 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 
+import src.utilities as U
+
 batch_size = 128
 num_classes = 10
 epochs = 12
@@ -50,10 +52,10 @@ model.add(Conv2D(32, kernel_size=(3, 3),
                  input_shape=input_shape))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+#model.add(Dropout(0.25))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
+#model.add(Dropout(0.5))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss=keras.losses.categorical_crossentropy,
@@ -68,3 +70,5 @@ model.fit(x_train, y_train,
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
+fname = U.gen_save_name('save/mnist_cnn_no_drop_run.h5')
+model.save(fname)
